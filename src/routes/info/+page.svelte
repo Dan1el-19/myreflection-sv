541cba761547f47b488b29004438257dc2c2a350
Dan1el-19 <daniel@blokserwis.pl>
2025-09-12 22:13:40 +0200

    Init

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,30 @@
+
+<script lang="ts">
+import ApiVersion from './ApiVersion.svelte';
+import BasicForm from './BasicForm.svelte';
+
+type FormData = {
+        name: string;
+        lastname: string;
+    };
+
+let receivedInfo = $state<FormData | null>(null);
+function handleFormSubmit(info: FormData) {
+    console.log("Dane formularza: ", info);
+    receivedInfo = info;
+}
+</script>
+<ApiVersion/>
+<main class="p-8 max-w-lg mx-auto">
+    <h1 class="text-3xl font-bold mb-6 text-center">Formularz Kontaktowy</h1>
+
+    <BasicForm onSubmit={handleFormSubmit} />
+
+    {#if receivedInfo}
+        <div class="mt-8 p-4 bg-green-100 border border-green-400 rounded-lg text-green-800">
+            <h2 class="text-xl font-bold">Otrzymano nowe dane!</h2>
+            <p class="mt-2"><strong>Imię:</strong> {receivedInfo.name}</p>
+            <p><strong>Nazwisko:</strong> {receivedInfo.lastname}</p>
+        </div>
+    {/if}
+</main>
---
+++ src/routes/info/ApiVersion.svelte
@@ -0,0 +1,4 @@
+<script>
+const version = "v1";
+</script>
+<h1 class="text-3xl text-blue-700 font-bold">API Version: {version}</h1>
